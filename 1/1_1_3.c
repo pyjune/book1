be16777c70565f2501073f3cb70ee952c2ee5f42
pyjune <pyjune@naver.com>
2019-05-26 23:57:56 +0900

    Create 1_1_3.c

---
+++ 1/1_1_3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+//int arr[1000000]; // 만약 N의 최대값이 100만인 경우
+
+int main(void)
+{
+	int T;
+	int N;
+	int arr[100];
+	freopen("input.txt", "r", stdin);  // 표준입력을 파일로 변경됨
+	scanf("%d", &T); // 테스트케이스 개수 T를 먼저 읽고
+	for (int tc = 1; tc <= T; tc++)  // T만큼 반복
+	{
+		scanf("%d", &N);
+		for (int i = 0; i < N; i++)
+		{
+			scanf("%d", &arr[i]);
+		}
+	}
+}
