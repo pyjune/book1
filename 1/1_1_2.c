5aa50aa43d26d6f4d7f9a4c7c7b6015f90d4fb99
pyjune <pyjune@naver.com>
2019-05-26 12:28:31 +0900

    Update 1_1_2.c

--- 1/1_1_2.c
+++ 1/1_1_2.c
@@ -2,11 +2,11 @@
 
 int main(void)
 {
-    int T;
-    fopen(“input.txt”, “r”, stdin);  // 표준입력을 파일로 변경됨
-    scanf("%d", &T); // 테스트케이스 개수 T를 먼저 읽고
-    for (int tc=1; tc<=T; tc++)  // T만큼 반복
-    {
-        // 이 부분에 N개의 정수를 읽고 최대값을 찾는 코드가 들어간다.
-    }
+	int T;
+	fopen("input.txt", "r", stdin);  // 표준입력을 파일로 변경됨
+	scanf("%d", &T); // 테스트케이스 개수 T를 먼저 읽고
+	for (int tc = 1; tc <= T; tc++)  // T만큼 반복
+	{
+		// 이 부분에 N개의 정수를 읽고 최대값을 찾는 코드가 들어간다.
+	}
 }
